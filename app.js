87ff0417866f4ce5ba7184088e1704a5ad06ace4
Alexander P <panshin.alexander@yahoo.com>
2021-08-30 23:51:07 +0300

    comm

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-var fs = require("fs");
-var http = require("http");
+var express = require('express')
+
+var app = express()
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html')
+})
+app.get('/getJson', function (req, res) {
+    res.sendFile(__dirname + '/testFile.json')
+})
 
 
-
-
-var server = http.createServer(function(request, response) {
-    console.log("URL " + request.url);
-    response.writeHead(200, {"Content-Type" : "text/html; charset=utf-8"});
-
-    var myReader = fs.createReadStream(__dirname + "/index.html")
-    myReader.pipe(response)
-    // myReader = fs.createReadStream(__dirname + "/testFile.json")
-    // myReader.pipe(response)
-});
-
-server.listen(3333, "127.0.0.1");
-console.log("LISTEN PORT 3333");
+app.listen(3333)
